0b232d8111d50d38f53546efaf98ac76a2169ff6
Gleb Bufin <gleb.bufin@paybis.com>
2026-03-13 11:28:11 +0200

    feat: add @vercel/analytics dependency and update favicon links in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,9 +33,23 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-icon-180x180.png" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-icon-180x180.png",
+        },
         { rel: "manifest", href: "/manifest.json" },
       ],
     },
@@ -43,7 +57,7 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss() as any],
   },
-  modules: ["@nuxtjs/google-fonts"],
+  modules: ["@nuxtjs/google-fonts", "@vercel/analytics"],
   googleFonts: {
     display: "swap",
     preload: true,
